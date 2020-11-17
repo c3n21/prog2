50b861c45ded55015bcb2927aedc1763266eeae8
Zhi Fan Chen <nezuko@pop-os.localdomain>
2020-11-17 10:47:01 +0100

    Created gitignore for javadoc, Map.put implemented

--- Map/Map.java
+++ Map/Map.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * <pre>
  * [OVERVIEW]
  *      Classe che rappresenta una mappa che ha come chiave una stringa, 
  *      e valore un intero.
@@ -10,7 +11,7 @@ import java.util.List;
  *      {@link put}
  *      {@link remove}
  *      {@link get}
- *
+ * </pre>
  * */
 
 public class Map {
@@ -23,6 +24,7 @@ public class Map {
     private List <Integer> values;
 
     /**
+     * <pre>
      * [AF]
      *      AF(keys, values) = values[key] dove "key" appartiene all'insieme delle stringhe chiavi keys {x1 ... xn} con xi non ridondante
      *          e "values" una lista di interi associati alla chiave key (due chiavi diverse possono avere lo stesso value)
@@ -35,14 +37,18 @@ public class Map {
      *                         
      * [AI]
      *      per qualsiasi elemento in keys, non ci devono essere duplicati
+     * </pre>
      * */
 
 
     /**
+     * <pre>
      * [CONSTRUCTOR]
+     * </pre>
      * */
 
     /**
+     * <pre>
      * [RI PRESERVATION]
      *      keys e values vengono istanziati, quindi hanno la stessa dimensione che è 0
      *
@@ -54,6 +60,7 @@ public class Map {
      *      keys.size   >= 0
      *      values.size >= 0
      *      keys.size   == values.size
+     * </pre>
      * */
     public Map () {
         keys   = new ArrayList<>();
@@ -64,10 +71,78 @@ public class Map {
      * [METHODS]
      * */
 
-    public void put (String key, int value) {
+    /**
+     * <pre>
+     * [RI PRESERVATION]
+     *      key viene aggiunto <==> key non appartiene a keys
+     *      se key viene aggiunto => value viene aggiunto, quindi 
+     *      {@code values.size()} == {@code keys.size()}
+     *
+     * [OP CORRECTNESS]
+     *      AF(keys, values) = values[key] con {@code key} appartenente a {@code keys}
+     *                         con {@code keys.size == values.size}
+     *                         {@code keys.add(key) => values.add(value)}
+     *
+     * [AI PRESERVATION]
+     *      keys.size   {@code >= 0}
+     *      values.size {@code >= 0}
+     *      keys.size   == values.size
+     *
+     * [MODIFIES]
+     *      {@code Map.keys}
+     *      {@code Map.values}
+     *
+     * [EFFECTS]
+     *      {@code Inserisce value nella map}
+     *      restituisce true se è stato possibile aggiungerlo
+     *      false altrimenti (la chiave e' gia' utilizzata)
+     *
+     * @param key
+     *
+     * @param value
+     *
+     * @throws IllegalArgumentException se @param key {@code e' gia' presente}
+     * </pre>
+     **/
+    public boolean put (String key, int value) {
+        if (keys.contains(key)) {
+            return false;
+        }
+        
+        keys.add(key);
+        values.add(value);
+
+        return true;
     }
 
+    /**
+     * <pre>
+     * [RI PRESERVATION]
+     *      key viene aggiunto <==> key non appartiene a keys
+     *      se key viene aggiunto => value viene aggiunto, quindi 
+     *      {@code values.size()} == {@code keys.size()}
+     *
+     * [OP CORRECTNESS]
+     *      AF(keys, values) = values[key] con {@code key} appartenente a {@code keys}
+     *                         con {@code keys.size == values.size}
+     *                         {@code keys.add(key) => values.add(value)}
+     *
+     * [AI PRESERVATION]
+     *      keys.size   {@code >= 0}
+     *      values.size {@code >= 0}
+     *      keys.size   == values.size
+     *
+     * [MODIFIES]
+     *      {@code Map.keys}
+     *      {@code Map.values}
+     *
+     * [EFFECTS]
+     *      {@code rimuove value associato alla chiave key se quest'ultima e' presente}
+     *
+     * </pre>
+     **/
     public void remove (String key) {
+        
     }
 
     public int get (String key) {
