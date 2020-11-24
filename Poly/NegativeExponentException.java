41cd065fd942d0fcf7c048d3a0582cfe34b87100
Zhi Fan Chen <nezuko@pop-os.localdomain>
2020-11-24 08:26:41 +0100

    Removed import

--- Poly/NegativeExponentException.java
+++
@@ -1,7 +0,0 @@
-public class NegativeExponentException extends RuntimeException {
-        public NegativeExponentException () {super();};
-
-        public NegativeExponentException (String msg) {
-                super(msg);
-        } 
-}
